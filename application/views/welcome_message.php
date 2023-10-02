659cb211167d2f8a13a08d37127142791f48203d
nafilah17 <rissanafilah@gmail.com>
2023-10-02 18:28:14 +0700

    remove bootstrap template

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -81,7 +81,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <div id="container">
 	<h1>Welcome !!</h1>
 
-	<!-- <div id="body">
+	<div id="body">
 		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
 
 		<p>If you would like to edit this page you'll find it located at:</p>
@@ -91,7 +91,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		<code>application/controllers/Welcome.php</code>
 
 		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="userguide3/">User Guide</a>.</p>
-	</div> -->
+	</div>
 
 	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
 </div>
